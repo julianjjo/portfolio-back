5c4d1c90ef0a432755c18d8bc931406d94f22d2d
Julian Mican <julian.mican@gmail.com>
2024-04-08 16:38:44 -0500

    Add configuration for cors

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,27 @@ pub mod api;
 extern crate rocket;
 use api::contact_me_api::create_contact_me;
 use repository::mongodb_repo::MongoRepo;
-use rocket::{get, http::Status, serde::json::Json, Request};
+use rocket::{get, http::Status, serde::json::Json, http::Header, Response, Request};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "https://julian-dev.dev"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[get("/")]
 fn hello() -> Result<Json<String>, Status> {
@@ -21,7 +41,7 @@ fn internal_error(req: &Request) -> String {
 #[launch]
 fn rocket() -> _ {
     let db = MongoRepo::init();
-    rocket::build().manage(db).register("/", catchers![internal_error])
+    rocket::build().attach(CORS).manage(db).register("/", catchers![internal_error])
         .mount("/", routes![hello])
         .mount("/", routes![create_contact_me])
 }
